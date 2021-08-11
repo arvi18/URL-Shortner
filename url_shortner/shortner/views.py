8a5e22090e925342457bdf9807c958d9d6d7f204
Arvind Shelke <ucdonor5@gmail.com>
2021-08-11 16:45:50 +0530

    C:/Program Files/Git/pk

--- url_shortner/shortner/views.py
+++ url_shortner/shortner/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 import uuid
 from .models import Url
 
@@ -8,8 +8,11 @@ def index(request):
 def create(request):
     if request.method=='POST':
         url=request.POST['link']
-        uid=str(uuid.uuid4()[:5])
+        uid=str(uuid.uuid4())[:5]
         new_url=Url(link=url, uuid=uid)
         new_url.save()
         return HttpResponse(uid)
-    
+
+def go(request, pk):
+    url_details=Url.objects.get(uuid=pk)
+    return redirect(url_details.link)
